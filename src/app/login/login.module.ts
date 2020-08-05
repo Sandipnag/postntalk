58143dc12e90c40f68cef919e4ba8003ddcb0fcc
Sandip Nag <sandipnag@Mac-mini.local>
2020-08-06 01:04:40 +0530

    branch change commit

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,3 +1,4 @@
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -12,7 +13,8 @@ import { LoginComponent } from './login.component';
     CommonModule,
     LoginRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxSpinnerModule
   ],
 })
 export class LoginModule { }
